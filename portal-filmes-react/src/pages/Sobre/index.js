2202ec483bd50de79db9e9143498c080dcb3f079
Pedro Vilaça <pedrovilacav@gmail.com>
2020-11-27 02:59:25 -0300

    Ajustada pagina Sobre (Exibição detalhada do destaque)

--- portal-filmes-react/src/pages/Sobre/index.js
+++ portal-filmes-react/src/pages/Sobre/index.js
@@ -16,12 +16,14 @@ function App(props) {
                 <div className="row">
                     <img src={'https://image.tmdb.org/t/p/w500/' + props.location.state.filme.poster_path} alt="" />
                 </div>
-                <h1>{props.location.state.filme.title}</h1>
-                <p>{props.location.state.filme.overview}</p>
-                <p> <b>Categoria: </b> {props.location.state.filme.category}</p>
-                <p> <b>Datade Lançamento:</b> {props.location.state.filme.release_date}</p>
-                <p> <b>Popularidade</b> {props.location.state.filme.popularity.toFixed(2) + ' %'}</p>
-                <p> <b>Avaliação</b> {props.location.state.filme.vote_average + ' / 10'}</p>
+                <div className="row">
+                    <h1>{props.location.state.filme.title}</h1>
+                    <p>{props.location.state.filme.overview}</p>
+                    <p> <b>Categoria: </b> {props.location.state.filme.category}</p>
+                    <p> <b>Datade Lançamento:</b> {props.location.state.filme.release_date}</p>
+                    <p> <b>Popularidade</b> {props.location.state.filme.popularity.toFixed(2) + ' %'}</p>
+                    <p> <b>Avaliação</b> {props.location.state.filme.vote_average + ' / 10'}</p>
+                </div>
 
             </div>
         </div>
